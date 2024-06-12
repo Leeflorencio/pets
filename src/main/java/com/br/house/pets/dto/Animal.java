5699f98bd4440eb0ada8938e7b6a15af5192b990
Leeflorencio <florenciolee63@gmail.com>
2024-06-11 21:46:42 -0300

    criando classe Pessoa, Animal e Endereco

--- src/main/java/com/br/house/pets/dto/Animal.java
+++ src/main/java/com/br/house/pets/dto/Animal.java
@@ -13,4 +13,82 @@ public class Animal {
     private double peso;
     private StatusAdocao statusAdocao;
 
+    public Animal(Long id, String tipo, String raca, Integer idade, PetSexo sexo, double peso, StatusAdocao statusAdocao) {
+        this.id = id;
+        this.tipo = tipo;
+        this.raca = raca;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.peso = peso;
+        this.statusAdocao = statusAdocao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public PetSexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(PetSexo sexo) {
+        this.sexo = sexo;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public StatusAdocao getStatusAdocao() {
+        return statusAdocao;
+    }
+
+    public void setStatusAdocao(StatusAdocao statusAdocao) {
+        this.statusAdocao = statusAdocao;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "id=" + id +
+                ", tipo='" + tipo + '\'' +
+                ", raca='" + raca + '\'' +
+                ", idade=" + idade +
+                ", sexo=" + sexo +
+                ", peso=" + peso +
+                ", statusAdocao=" + statusAdocao +
+                '}';
+    }
 }
---
+++ src/main/java/com/br/house/pets/dto/Pessoa.java
@@ -0,0 +1,81 @@
+package com.br.house.pets.dto;
+
+public class Pessoa {
+
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String email;
+    private Endereco endereco;
+
+    public Pessoa(Long id, String nome, String cpf, String telefone, String email, String rua, int numero, String complemento, String bairro, String cidade, String estado, String cep) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+        this.endereco = new Endereco(rua, numero, complemento, bairro, cidade, estado, cep);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", email='" + email + '\'' +
+                ", endereco=" + endereco +
+                '}';
+    }
+
+}
